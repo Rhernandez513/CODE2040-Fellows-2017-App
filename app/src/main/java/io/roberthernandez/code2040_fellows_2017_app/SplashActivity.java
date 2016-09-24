3d4d96913ef233db0ec8803c382e7c9d0ccd5b1a
Robert David Hernandez <rhernandez513@gmail.com>
2016-09-25 03:45:06 +0800

    add a splash screen

---
+++ app/src/main/java/io/roberthernandez/code2040_fellows_2017_app/SplashActivity.java
@@ -0,0 +1,20 @@
+package io.roberthernandez.code2040_fellows_2017_application;
+
+import io.roberthernandez.code2040_fellows_2017_app.MainActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+
+/**
+ * Created by robert on 9/9/16.
+ */
+public class SplashActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
